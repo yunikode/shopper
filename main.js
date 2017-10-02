fb1be977c3c2a8352738ec55d87fda909761e2d9
zee-german <franz.lutzenberger@gmail.com>
2017-10-01 20:22:52 -0400

    cleanup and fix stupid

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const electron = require('electron')
 
-const {app, BrowserWindow, Menu, ipcMain} = electron
+const {app, BrowserWindow, webContents, Menu, ipcMain} = electron
 
 const path = require('path')
 const url = require('url')
 
+const PouchDB = require('pouchdb')
+PouchDB.plugin(require('pouchdb-find'))
+const db = new PouchDB('my_shopping')
+
 let mainWindow
 let addWindow
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   mainWindow.loadURL(url.format({
@@ -21,7 +25,7 @@ function createWindow () {
 
   Menu.setApplicationMenu(mainMenu)
 
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -30,29 +34,75 @@ function createWindow () {
   })
 }
 
-ipcMain.on('item:add', function (e, item) {
-  if (item.length != 0) {
-    mainWindow.webContents.send('item:add', item)
+ipcMain.on('form:open', function(e) {
+  openAddWindow()
+})
+
+ipcMain.on('item:delete', (e, item) => {
+  db.find({
+    selector: {
+      title: {
+        $eq: item
+      }
+    }
+  }).then(function(res) {
+    for (var i = 0; i < res.docs.length; i++) {
+      let id = res.docs[i]._id
+      db.get(id).then(function(doc) {
+        db.remove(doc)
+        return refreshItems()
+      })
+    }
+  }).catch(err => console.log(err))
+
+})
+
+ipcMain.on('item:add', function(e, item) {
+
+  let _item = {
+    _id: new Date().toISOString(),
+    title: item,
+    completed: false
   }
+  db.put(_item, function cb(err, res) {
+    if (!err) {
+      console.log('Successfully added item!')
+    }
+  })
+
+  refreshItems()
+
   addWindow.close()
 })
 
-app.on('ready', createWindow)
+function refreshItems() {
+  db.allDocs({include_docs: true, attachments: true}).then(items => {
+    mainWindow.webContents.send('list:repopulate', items)
+  }).catch(err => console.log(err));
+}
 
-app.on('window-all-closed', function () {
+app.on('ready', () => {
+  createWindow()
+  ipcMain.on('mainWindowLoaded', () => {
+    console.log('DOMReady')
+    refreshItems()
+  })
+})
+
+app.on('window-all-closed', function() {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
-app.on('activate', function () {
+app.on('activate', function() {
   if (mainWindow === null) {
     createWindow()
   }
 })
 
 function openAddWindow() {
-  addWindow = new BrowserWindow({width: 300, height: 200, frame:false})
+  addWindow = new BrowserWindow({width: 300, height: 200, frame: false})
 
   addWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'add.html'),
@@ -60,7 +110,7 @@ function openAddWindow() {
     slashes: true
   }))
 
-  addWindow.on('closed', function () {
+  addWindow.on('closed', function() {
     addWindow = null
   })
 }
@@ -75,16 +125,27 @@ const menuTemplate = [
         click() {
           openAddWindow()
         }
-      },
-      {
+      }, {
         label: 'Clear Item',
+        accelerator: process.platform == 'darwin'
+          ? 'Command+Alt+X'
+          : 'Ctrl+Alt+X',
         click() {
           mainWindow.webContents.send('item:clear')
         }
-      },
-      {
+      }, {
+        label: 'Help',
+        accelerator: process.platform == 'darwin'
+          ? 'Command+H'
+          : 'Ctrl+H',
+        click() {
+          mainWindow.webContents.send('help:show')
+        }
+      }, {
         label: 'Quit',
-        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        accelerator: process.platform == 'darwin'
+          ? 'Command+Q'
+          : 'Ctrl+Q',
         click() {
           app.quit()
         }
@@ -103,12 +164,13 @@ if (process.env.NODE_ENV !== 'production') {
     submenu: [
       {
         label: 'Toggle Dev Tools',
-        accelerator: process.platform == 'darwin' ? 'Command+Shift+I' : 'Ctrl+Shift+I',
+        accelerator: process.platform == 'darwin'
+          ? 'Command+Shift+I'
+          : 'Ctrl+Shift+I',
         click(item, focusedWindow) {
           focusedWindow.toggleDevTools()
         }
-      },
-      {
+      }, {
         role: 'reload'
       }
     ]
--- renderer.js
+++ renderer.js
@@ -1,6 +1,9 @@
 const electron = require('electron')
 const {ipcRenderer} = electron
 
+
+const add_btn = document.querySelector('#add-button')
+const close_btn = document.getElementById('close-btn')
 const form = document.querySelector('form')
 const list = document.querySelector('#item-list')
 if (form !== null) {
@@ -8,6 +11,11 @@ if (form !== null) {
 }
 
 if (list !== null) {
+  add_btn.addEventListener('click', openForm)
+  close_btn.addEventListener('click', function (e) {
+    e.preventDefault()
+    toggleShow()
+  })
   list.addEventListener('dblclick', removeItem)
   list.addEventListener('click', toggleItem)
 }
@@ -15,29 +23,59 @@ if (list !== null) {
 function submitForm(e) {
   e.preventDefault()
   const item = document.querySelector('#item').value.trim()
-  ipcRenderer.send('item:add', item)
+  if (item.length != 0) {
+    ipcRenderer.send('item:add', item)
+  }
 }
 
-ipcRenderer.on('item:add', function (e, item) {
-  list.className = 'collection'
-  let li = document.createElement('li')
-  li.className = 'collection-item'
-  let text = document.createTextNode(item)
-  li.appendChild(text)
-  list.appendChild(li)
+function openForm(e) {
+  ipcRenderer.send('form:open', e)
+}
+
+ipcRenderer.on('app:ready', (event, message) => {
+  document.getElementById('head').innerHTML = `${message}`
 })
+
+ipcRenderer.on('list:repopulate', (e, payload) => {
+  let i = payload.rows.length
+  console.log('redraw requested')
+  // console.log(i)
+
+  list.innerHTML = ''
+
+  for (item = 0; item < i; item++) {
+
+    list.className = 'collection'
+    let li = document.createElement('li')
+    li.className = 'collection-item'
+    let text = document.createTextNode(payload.rows[item].doc.title)
+    li.appendChild(text)
+    list.appendChild(li)
+  }
+
+})
+
 
 ipcRenderer.on('item:clear', function () {
   list.innerHTML = ''
   list.classList.toggle('collection')
 })
 
+ipcRenderer.on('help:show', function () {
+  toggleShow()
+})
+
+function toggleShow(e) {
+  document.getElementById('help-screen').classList.toggle('no-show')
+}
+
 function toggleItem(e) {
   e.target.classList.toggle('done')
 }
 
 function removeItem(e) {
-  e.target.remove()
+  let _item = e.target.innerHTML
+  ipcRenderer.send('item:delete', _item)
   if (list.innerHTML.trim().length == 0) {
     list.classList.toggle('collection')
   }
